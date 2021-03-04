65b6254367d532e28f4e8f367fec45ffe1a1e9bd
Sang Hyup Lee <tms.lee9@gmail.com>
2021-03-04 01:34:25 +0000

    add admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  
   def show
     @num_product = Product.count
     @num_cat = Category.count
